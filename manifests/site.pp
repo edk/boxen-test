686dfe7a46235d1848e64862c30979cff0b76737
Eddy Kim <eddyhkim@gmail.com>
2014-11-18 15:21:53 -0800

    move stuff out of site.pp to coupa::environment.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -89,33 +89,4 @@ node default {
     target => $boxen::config::repodir
   }
 
-  include atom
-  include sublime_text_2
-  include iterm2::dev
-  include mysql
-  include firefox
-  include virtualbox
-  include vagrant
-  include chrome
-  #include karabiner
-  include skype
-  include redis
-  include imagemagick
-  include dash
-  include hipchat
-  #include slack
-  include memcached
-  include wkhtmltopdf
-  include sequel_pro
-  include macvim
-  include swig
-  include pcre
-  include qt
-  #include osx
-  include graphviz
-  include gpgme
-  include screen
-  include tmux
-  include textmate
-  include gitx
 }
---
+++ modules/coupa/manifests/environment.pp
@@ -0,0 +1,32 @@
+
+class coupa::environment {
+  include atom
+  include sublime_text_2
+  include iterm2::dev
+  include mysql
+  include firefox
+  include virtualbox
+  include vagrant
+  include chrome
+  #include karabiner
+  include skype
+  include redis
+  include imagemagick
+  include dash
+  include hipchat
+  #include slack
+  include memcached
+  include wkhtmltopdf
+  include sequel_pro
+  include macvim
+  include swig
+  include pcre
+  include qt
+  #include osx
+  include graphviz
+  include gpgme
+  include screen
+  include tmux
+  include textmate
+  include gitx
+}
